2db6a6cba7371a0bb692e9b5514d02d4f93bb109
MLN <mlnkrish86@gmail.com>
2014-08-22 12:48:26 +0530

    validations and saving multiple receipts & vouchers

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,22 +2,50 @@ class TransactionsController < ApplicationController
   def new
   end
 
-  # {"receiptInfo"=>[{"storeName"=>"StoreName", "amount"=>"1000", "billNo"=>"AWE", "isToday"=>false}], "customerInfo"=>{"mobile"=>"9887887878", "name"=>"MLN", "email"=>"mln@tw.cc", "gender"=>"M", "age"=>"112", "occupation"=>"Dev", "address"=>"12, B Main, elsewhere"}, "voucherInfo"=>[{"barCode"=>"QWEEW", "scratchCode"=>"YUUI"}]}
   def create
     body = JSON.parse(request.body.read)
 
     customer_info = body["customerInfo"] 
-    receipt_info = body["receiptInfo"][0]
-    voucher_info = body["voucherInfo"][0] 
+    receipt_info = body["receiptInfo"]
+    voucher_info = body["voucherInfo"]
 
-    customer = Customer.new(name: customer_info["name"], email: customer_info["email"], mobile: customer_info["mobile"], address: customer_info["address"], occupation: customer_info["occupation"], gender: customer_info["gender"], age: customer_info["age"])
-    transaction = customer.transactions.new date: Date.today
-    store = Store.new(name:receipt_info["storeName"])
-    receipt = transaction.transaction_items.new(item_id: receipt_info["billNo"], store: store, amount: receipt_info["amount"]         , date: Date.today)
-    voucher = transaction.vouchers.new(barcode_number: voucher_info["barCode"], scratch_code: voucher_info["scratchCode"])
+    total_receipt_value = receipt_info
+                          .map{|h| h["amount"].to_i}
+                          .inject{|total, val| total+val}
+
+    if(total_receipt_value < 1000)
+       render :nothing => true, :status => 400
+       return
+    end
+
+    no_of_vouchers_to_issue = total_receipt_value / 1000
+
+    if(voucher_info.length != no_of_vouchers_to_issue)
+      render :nothing => true, :status => 400
+      return
+    end
+
+    customer = Customer.find_by_mobile(customer_info["mobile"])
+    if(customer.nil?)
+      customer = Customer.new(name: customer_info["name"], email: customer_info["email"], mobile: customer_info["mobile"], address: customer_info["address"], occupation: customer_info["occupation"], gender: customer_info["gender"], age: customer_info["age"])
+    end
+    
+    transaction = customer.transactions.new date:Date.today
+
+    receipt_info.each do |receipt|
+      store = Store.find_by_name(receipt["storeName"])
+      if(store.nil?)
+        render :nothing => true, :status => 400
+        return
+      end
+      transaction.transaction_items.new(item_id: receipt["billNo"], store: store, amount: receipt["amount"], date: Date.today)
+    end
+    
+    voucher_info.each do |voucher|
+      transaction.vouchers.new(barcode_number: voucher["barCode"], scratch_code: voucher["scratchCode"])
+    end
 
     customer.save!
-
     
     render :nothing => true
   end
--- spec/controllers/transaction_controller_spec.rb
+++ spec/controllers/transaction_controller_spec.rb
@@ -1,10 +1,22 @@
 require "rails_helper"
 
 describe TransactionsController do
+
+  before(:each) do
+    store = Store.create name:"Cookie Jar"
+    store = Store.create name:"Monkey Bar"
+    store = Store.create name:"Donkey Car"
+  end
   
   it "Creates a transaction with one receipt" do
 
-    post :create, '{"receiptInfo":[{"storeName":"StoreName","amount":"1000","billNo":"AWE","isToday":false}],"customerInfo":{"mobile":"9887887878","name":"MLN","email":"mln@tw.cc","gender":"M","age":"112","occupation":"Dev","address":"12, B Main, elsewhere"},"voucherInfo":[{"barCode":"QWEEW","scratchCode":"YUUI"}]}'
+    post :create, '{"receiptInfo":[
+                        {"storeName":"Cookie Jar","amount":"1000","billNo":"AWE","isToday":true}
+                    ],
+                    "customerInfo":{"mobile":"9887887878","name":"MLN","email":"mln@tw.cc","gender":"M","age":"112","occupation":"Dev","address":"12, B Main, elsewhere"},
+                    "voucherInfo":[
+                        {"barCode":"QWEEW","scratchCode":"YUUI"}
+                    ]}'
 
     Customer.all.length.should == 1
     customer = Customer.first
@@ -34,8 +46,154 @@ describe TransactionsController do
     voucher.barcode_number.should == "QWEEW"
     voucher.scratch_code.should == 'YUUI'
   end
-
-
   
+  it "Creates a transaction with multiple receipts" do
+
+    post :create, '{"receiptInfo":[
+                        {"storeName":"Cookie Jar","amount":"500","billNo":"AWE1", "isToday":true},
+                        {"storeName":"Monkey Bar","amount":"400","billNo":"AWE2", "isToday":true},
+                        {"storeName":"Donkey Car","amount":"100","billNo":"AWE3", "isToday":true}
+                    ],
+                    "customerInfo":{"mobile":"9887887878","name":"MLN","email":"mln@tw.cc","gender":"M","age":"112","occupation":"Dev","address":"12, B Main, elsewhere"},
+                    "voucherInfo":[
+                        {"barCode":"QWEEW","scratchCode":"YUUI"}
+                    ]}'
+
+    Customer.all.length.should == 1
+    customer = Customer.first
+    customer.transactions.length.should == 1
+    transaction = customer.transactions.first
+    transaction.transaction_items.length.should == 3
+    transaction.vouchers.length.should == 1
+  end
+  
+  it "Creates a transaction with multiple vouchers" do
+
+    post :create, '{"receiptInfo":[
+                        {"storeName":"Cookie Jar","amount":"1000","billNo":"1", "isToday":true},
+                        {"storeName":"Monkey Bar","amount":"1000","billNo":"2", "isToday":true},
+                        {"storeName":"Donkey Car","amount":"1000","billNo":"3", "isToday":true}
+                    ],
+                    "customerInfo":{"mobile":"9887887878","name":"MLN","email":"mln@tw.cc","gender":"M","age":"112","occupation":"Dev","address":"12, B Main, elsewhere"},
+                    "voucherInfo":[
+                        {"barCode":"1","scratchCode":"1"},
+                        {"barCode":"2","scratchCode":"2"},
+                        {"barCode":"3","scratchCode":"3"}
+                    ]}'
+
+    Customer.all.length.should == 1
+    customer = Customer.first
+    customer.transactions.length.should == 1
+    transaction = customer.transactions.first
+    transaction.transaction_items.length.should == 3
+    transaction.vouchers.length.should == 3
+  end
+  
+  it "Rejects if total receipt value is less than 1000" do
+    post :create, '{"receiptInfo":[
+                        {"storeName":"Cookie Jar","amount":"100","billNo":"1", "isToday":true},
+                        {"storeName":"Monkey Bar","amount":"200","billNo":"2", "isToday":true},
+                        {"storeName":"Donkey Car","amount":"699","billNo":"3", "isToday":true}
+                    ],
+                    "customerInfo":{"mobile":"9887887878","name":"MLN","email":"mln@tw.cc","gender":"M","age":"112","occupation":"Dev","address":"12, B Main, elsewhere"},
+                    "voucherInfo":[
+                        {"barCode":"1","scratchCode":"1"},
+                        {"barCode":"2","scratchCode":"2"},
+                        {"barCode":"3","scratchCode":"3"}
+                    ]}'
+
+    expect(response).to have_http_status(:bad_request)
+
+    Customer.all.length.should == 0
+  end
+  
+  it "Fails when less vouchers are issued" do
+    post :create, '{"receiptInfo":[
+                        {"storeName":"Cookie Jar","amount":"100","billNo":"1", "isToday":true},
+                        {"storeName":"Monkey Bar","amount":"1200","billNo":"2", "isToday":true},
+                        {"storeName":"Donkey Car","amount":"1000","billNo":"3", "isToday":true}
+                    ],
+                    "customerInfo":{"mobile":"9887887878","name":"MLN","email":"mln@tw.cc","gender":"M","age":"112","occupation":"Dev","address":"12, B Main, elsewhere"},
+                    "voucherInfo":[
+                        {"barCode":"1","scratchCode":"1"}
+                    ]}'
+
+    expect(response).to have_http_status(:bad_request)
+
+    Customer.all.length.should == 0
+  end
+  
+  it "Fails when more vouchers are issued" do
+    post :create, '{"receiptInfo":[
+                        {"storeName":"Cookie Jar","amount":"100","billNo":"1", "isToday":true},
+                        {"storeName":"Monkey Bar","amount":"1200","billNo":"2", "isToday":true},
+                        {"storeName":"Donkey Car","amount":"1000","billNo":"3", "isToday":true}
+                    ],
+                    "customerInfo":{"mobile":"9887887878","name":"MLN","email":"mln@tw.cc","gender":"M","age":"112","occupation":"Dev","address":"12, B Main, elsewhere"},
+                    "voucherInfo":[
+                        {"barCode":"1","scratchCode":"1"},
+                        {"barCode":"2","scratchCode":"2"},
+                        {"barCode":"3","scratchCode":"3"},
+                        {"barCode":"4","scratchCode":"4"},
+                        {"barCode":"5","scratchCode":"5"}
+                    ]}'
+
+    expect(response).to have_http_status(:bad_request)
+
+    Customer.all.length.should == 0
+  end
+  
+  it "Attach new transaction to an existing customer" do
+
+    post :create, '{"receiptInfo":[
+                        {"storeName":"Cookie Jar","amount":"1000","billNo":"1", "isToday":true},
+                        {"storeName":"Monkey Bar","amount":"1000","billNo":"2", "isToday":true}
+                    ],
+                    "customerInfo":{"mobile":"9887887878","name":"MLN","email":"mln@tw.cc","gender":"M","age":"112","occupation":"Dev","address":"12, B Main, elsewhere"},
+                    "voucherInfo":[
+                        {"barCode":"1","scratchCode":"1"},
+                        {"barCode":"2","scratchCode":"2"}
+                    ]}'
+
+    post :create, '{"receiptInfo":[
+                        {"storeName":"Cookie Jar","amount":"1200","billNo":"3", "isToday":true},
+                        {"storeName":"Monkey Bar","amount":"1020","billNo":"4", "isToday":true},
+                        {"storeName":"Donkey Car","amount":"1000","billNo":"5", "isToday":true}
+                    ],
+                    "customerInfo":{"mobile":"9887887878","name":"MLN","email":"mln@tw.cc","gender":"M","age":"112","occupation":"Dev","address":"12, B Main, elsewhere"},
+                    "voucherInfo":[
+                        {"barCode":"3","scratchCode":"3"},
+                        {"barCode":"4","scratchCode":"4"},
+                        {"barCode":"5","scratchCode":"5"}
+                    ]}'
+
+    Customer.all.length.should == 1
+    customer = Customer.first
+    customer.transactions.length.should == 2
+    
+    transaction = customer.transactions[0]
+    transaction.transaction_items.length.should == 2
+    transaction.vouchers.length.should == 2
+
+    transaction = customer.transactions[1]
+    transaction.transaction_items.length.should == 3
+    transaction.vouchers.length.should == 3
+  end
+
+  it "Fails when unknown store is given" do
+
+    post :create, '{"receiptInfo":[
+                        {"storeName":"Monkey Bar","amount":"1000","billNo":"AWE3", "isToday":true},
+                        {"storeName":"Donkey Tar","amount":"1000","billNo":"AWE3", "isToday":true}
+                    ],
+                    "customerInfo":{"mobile":"9887887878","name":"MLN","email":"mln@tw.cc","gender":"M","age":"112","occupation":"Dev","address":"12, B Main, elsewhere"},
+                    "voucherInfo":[
+                        {"barCode":"QWEEW","scratchCode":"YUUI"}
+                    ]}'
+
+    expect(response).to have_http_status(:bad_request)
+
+    Customer.all.length.should == 0
+  end
 
 end
