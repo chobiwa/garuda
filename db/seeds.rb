be4215f2eb3c27dec2f1ca280e9ca6bd0578b971
MLN <mlnkrish86@gmail.com>
2014-08-26 11:54:26 +0530

    seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,14 @@ TransactionItem.delete_all
 Voucher.delete_all
 User.delete_all
 
-Customer.create! name: "Chobi", email: "chobi@goo.com", mobile: "9611805469", address: "20, blah, blah", occupation: "Blah", gender: "M", age: 78
 
 user = User.new(:email => 'mln@tws.com', :password => 'password', :password_confirmation => 'password', :name => "MLN Krishnan")
+user.save!
+user = User.new(:email => 'user1@email.com', :password => 'password', :password_confirmation => 'password', :name => "User1")
+user.save!
+user = User.new(:email => 'user2@email.com', :password => 'password', :password_confirmation => 'password', :name => "user2")
+user.save!
+user = User.new(:email => 'user3@email.com', :password => 'password', :password_confirmation => 'password', :name => "User2")
 user.save!
 
 
