252502e15cdda70797440c4e5455e571305a4678
Saheli <sahelig@thoughtworks.com>
2014-09-01 11:55:50 +0530

    Adding link to see customer details

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,15 +2,24 @@ class CustomersController < ApplicationController
   before_action :require_admin_login, only: :index
   before_action :authenticate_user! 
 
+
   def show
     mobile = params[:id]
-    c = Customer.find_by_mobile(mobile)
-    if(c.nil?)
+    @c = Customer.find_by_mobile(mobile)
+    if(@c.nil?)
       render :nothing => true, :status => :not_found
       return
     end
-    json = c.to_json ({:methods => :is_winner?})
-    render :json => json
+    @t = @c.transactions
+    respond_to do |format|
+      format.json do
+        json = @c.to_json ({:methods => :is_winner?})
+        render :json => json
+      end
+      format.html do
+      
+      end
+    end
   end
 
   def index
